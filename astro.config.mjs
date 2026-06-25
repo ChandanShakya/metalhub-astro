60a6daa4f819c32e1a9b9dcc89dc1324d9c91d5f
Chandan Shakya <chandan.shakya@nccs.edu.np>
2026-06-25 12:28:20 +0545

    feat: formatting

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 // @ts-check
 
+import cloudflare from "@astrojs/cloudflare";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
-
-import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
     site: "https://metalhub.pages.dev",
